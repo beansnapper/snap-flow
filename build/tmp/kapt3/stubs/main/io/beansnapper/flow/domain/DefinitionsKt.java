98fb367e6c723126f2a9c307befde572575e1aad
beansnapper <kurtwguenther@gmail.com>
2020-12-14 14:09:41 -0500

    removing build and .gradle dirs

--- build/tmp/kapt3/stubs/main/io/beansnapper/flow/domain/DefinitionsKt.java
+++
@@ -1,7 +0,0 @@
-package io.beansnapper.flow.domain;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000*\u0016\u0010\u0000\"\b\u0012\u0004\u0012\u00020\u00020\u00012\b\u0012\u0004\u0012\u00020\u00020\u0001*\n\u0010\u0003\"\u00020\u00042\u00020\u0004\u00a8\u0006\u0005"}, d2 = {"ActionLambda", "Lkotlin/Function0;", "", "Timestamp", "Ljava/util/Date;", "snap-flow"})
-public final class DefinitionsKt {
-}
